b42f0f4cf191dfc2291abef8f4042fb161f2ca3b
Andrew Sutton <ajsutton94@gmail.com>
2020-07-09 13:44:12 -0500

    Add project files.

---
+++ PracticeLinq/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq;
+
+namespace PracticeLinq
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var carNames = new string[] { "Honda Civic", "Toyota Camry", "Ford Taurus", "Volkswagen Jetta", "Chevy Impala" };
+            var carsWithC = from cars in carNames
+                            where cars.ToLower().Contains("c")
+                            orderby cars descending
+                            select cars;
+            foreach (var car in carsWithC)
+            {
+                Console.WriteLine(car);
+            }
+            var videoGames = new string[] { "Skyrim", "Breath of the Wild", "Doom", "Crusader Kings II", "Dota2" };
+            var videoGamesByName = videoGames.OrderBy(game => game.Length);
+            foreach (var game in videoGamesByName)
+            {
+                Console.WriteLine(game);
+            }
+        }
+    }
+}
